0931ec0b3ce863146f44f94acbdf7e288cc958dc
SIDPAT67 <114729789+SIDPAT67@users.noreply.github.com>
2023-03-27 21:24:47 +0530

    Practicing Django

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Sid',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '15 March 2023'
+    },
+    {
+        'author': 'Cat',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '16 March 2023'
+    }
+]
+
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+    context = {
+        'posts': posts
+        }
+    return render(request, 'blog/home.html', context)
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
